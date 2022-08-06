b35a7cb9c2244d247e7bb589126664b99137f99c
Gonza <gonzagonzalezmiguel@gmail.com>
2022-08-06 12:48:46 +0200

    Fast mode implemented

--- v2.py
+++ v2.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from translate import Translator
 
 
@@ -14,10 +14,11 @@ def _help(update, context):
           '/start - Receive the welcome message\n' \
           '/help - Returns list of every AskTelegramBot command\n' \
           '/echo {sentence} - Returns the given sentence\n\n' \
-          'TRANSLATE COMMANDS\n\n' \
+          'TRANSLATION COMMANDS\n\n' \
           '/translate {sentence} - Translates the given sentence\n\n' \
           '/fromlanguage {language} - Updates the language which /translate will read. Language must be given in IETF language tag format\n\n' \
-          '/tolanguage {language} - Updates the language /translate will return. Language must be given in IETF language tag format'
+          '/tolanguage {language} - Updates the language /translate will return. Language must be given in IETF language tag format\n\n' \
+          '/fastmode - Activates/Deactivates fast mode, which allows the bot to translate every message you send without using /translate.'
     context.bot.send_message(update.message.chat_id, msg)
 
 
@@ -30,19 +31,29 @@ def translate(update, context):
     if from_ == '':
         from_ = update.message.from_user.language_code
     translator = Translator(from_lang=from_, to_lang=to_)
-    context.bot.send_message(update.message.chat_id, translator.translate(' '.join(context.args)))
+    if fast_mode:
+        context.bot.send_message(update.message.chat_id, translator.translate(update.message.text))
+    else:
+        if context.args:
+            context.bot.send_message(update.message.chat_id, translator.translate(' '.join(context.args)))
 
 
 def fromlanguage(update, context):
     global from_
     from_ = context.args[0]
-    context.bot.send_message(update.message.chat_id, 'Successfully updated.')
+    context.bot.send_message(update.message.chat_id, 'Language updated.')
 
 
 def tolanguage(update, context):
     global to_
     to_ = context.args[0]
-    context.bot.send_message(update.message.chat_id, 'Successfully updated.')
+    context.bot.send_message(update.message.chat_id, 'Language updated.')
+
+
+def fastmode(update, context):
+    global fast_mode
+    fast_mode = not fast_mode
+    context.bot.send_message(update.message.chat_id, 'Fast mode activated.')
 
 
 def launch_bot():
@@ -56,6 +67,8 @@ def launch_bot():
     dispatcher.add_handler(CommandHandler('translate', translate))
     dispatcher.add_handler(CommandHandler('fromlanguage', fromlanguage))
     dispatcher.add_handler(CommandHandler('tolanguage', tolanguage))
+    dispatcher.add_handler(CommandHandler('fastmode', fastmode))
+    dispatcher.add_handler(MessageHandler(Filters.text, translate))
     # Starts the bot
     updater.start_polling()
     # Bot nonstop hearing the channel
@@ -65,4 +78,5 @@ def launch_bot():
 if __name__ == '__main__':
     from_ = ''
     to_ = 'en'
+    fast_mode = False
     launch_bot()
